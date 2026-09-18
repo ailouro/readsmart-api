9694339bfd8f97199bd524f88488ef1c57776366
Lourene <lourenegutierrez2@gmail.com>
2026-09-18 15:26:31 +0800

    Add teacher reassignment functionality
    - Add reassignTeacher method in AdminWebController to securely update teacher_id for student accounts
    - Register admin.students.reassign-teacher POST route to resolve 500 dashboard error

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::post('/admin/teachers/{id}/revoke', [AdminWebController::class, 'revokeTe
 
 
 Route::post('/admin/users/{id}/reset-password', [AdminWebController::class, 'resetPassword'])->name('admin.users.reset-password');
+Route::post('/admin/students/{id}/reassign-teacher', [App\Http\Controllers\AdminWebController::class, 'reassignTeacher'])->name('admin.students.reassign-teacher');
