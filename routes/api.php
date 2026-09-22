f06b6842085c6e0f637406a553e9bb8752867764
Lourene <lourenegutierrez2@gmail.com>
2026-09-22 19:15:11 +0800

    Add route for storing individual assessments in AssessmentController

--- routes/api.php
+++ routes/api.php
@@ -56,6 +56,7 @@ Route::get('/teachers/{teacher_id}/mispronunciations', [ClassEnrollmentControlle
 Route::get('/teachers/{teacher_id}/classes', [ClassEnrollmentController::class, 'getTeacherClasses']);
 Route::get('/teachers/{teacher_id}/dashboard-summary', [ClassEnrollmentController::class, 'getTeacherDashboardSummary']);
 Route::post('classes/{classId}/assessments/bulk', [AssessmentController::class, 'bulkStore']);
+Route::post('classes/{classId}/assessments', [AssessmentController::class, 'store']);
 Route::post('/email/resend', [AuthController::class, 'resendVerification']);
 Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
 Route::post('/request-student-account', [AuthController::class, 'requestStudentAccount']);
